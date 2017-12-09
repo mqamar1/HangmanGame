59ef5142b9661e66483f67b8c2ee7806b49ad487
mqamar1 <sufimehvish@yahoo.com>
2017-12-09 01:42:18 -0500

    update

--- javascript/game.js
+++ javascript/game.js
@@ -1,68 +1,51 @@
 //Word bank
 var disneyMovies = ["Beauty and the Beast", "The Lion King", "Aladdin", "Snow White", "Toy Story", "Cinderella"];
-//Guessed letters
 var rightGuessLetters = [];
-//Used Letters
 var usedLetter = [];
-//Number of guesses left
 var numGuessesLeft = [];
-//computer's pick
-//var computerGuess = random_word();
 var selectedWord = ""
 var startOver = [];
 var wrongGuessLetters = [];
-//win and losses
 var win = 0;
 var loss = 0;
 var totalGuess = 20;
 var guess = ("");
 var blank_letters = [];
-//select random word from disney disneyMovies
-// window.onload = function(){
+
 randomWord();
-//
-// function randomword(){
-//   console.log("Hi")
-// }
-//
-// document.addEventListener("keyup", function(event){
-//   console.log("pressed")
-// })
+
 function randomWord() {
   var randomMovie = disneyMovies[Math.floor(Math.random() * disneyMovies.length)];
   console.log(randomMovie)
   var lowerCase = randomMovie.toLowerCase();
-  //console.log( "lowercase " + lowerCase);
+
   var spaceMovie = randomMovie.toLowerCase().split("");
-  //console.log(spaceMovie);
+
   selectedWord = spaceMovie;
   var numberLetters = spaceMovie.length;
   startOver = [];
 
   for (var i = 0; i < numberLetters; i++) {
     startOver.push("_");
-    //console.log(startOver);
+
   }
   totalGuess = 20;
-  // console.log(totalGuess);
+
   document.getElementById("guesses").innerHTML = totalGuess;
   document.getElementById("word").innerHTML = startOver.join(" ");
 
   function replaceBlanks(letter) {
     console.log("replaceBlanks")
     for (var i = 0; i < randomMovie.length; i++) {
-      //console.log(startOver[i])
-      //console.log(startOver.charAt());
+
       if (letter === lowerCase.charAt(i)) {
-        //console.log(randomMovie.charAt(i));
         startOver[i] = letter;
-        //console.log(startOver);//
         document.getElementById("word").innerHTML = startOver.join(' ');
 
       }
     }
   }
-  // todo: duplicate
+
   function dupLetters(keyPress) {
     for (var i = 0; i < startOver.length; i++) {
       if (keyPress === startOver[i]) {
@@ -70,10 +53,10 @@ function randomWord() {
         usedLetter.push(keyPress);
         document.getElementById("usedLetters").innerHTML = usedLetter.join(' ');
 
-        }
-
       }
+
     }
+  }
 
 
 
@@ -88,7 +71,7 @@ function randomWord() {
 
 
   document.onkeyup = function(event) {
-    //console.log(event);
+
     var keyPress = event.key.toLowerCase();
     console.log(" keyPress " + keyPress);
     var letterIndex = selectedWord.indexOf(keyPress);
@@ -99,8 +82,6 @@ function randomWord() {
     if (totalGuess > 0) {
       if (letterIndex >= 0) {
         console.log("Exists")
-        //  var total = totalGuess - letterIndex;
-        // console.log(total);
         replaceBlanks(keyPress, letterIndex)
         rightWord();
       }
